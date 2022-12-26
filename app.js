c1d8b5f9b17b7c46cd6316451703efcb6e627bef
MickieTH <knissayan@gmail.com>
2022-12-26 09:12:29 +0700

    delete comment in js

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// let h3 = document.querySelector('#dddmdy')
-
-// let d = new Date()
-// // console.log(d)
-
-// let dCut = String(d).split(" ")
-// // console.log(dCut)
-
-// h3.textContent = ` ${dCut[0]} ${dCut[2]} ${dCut[1]} ${dCut[3]} `
-
-// setInterval(timer, 1000)
-
-// function timer() {
-//     let h1 = document.querySelector('#time').innerHTML =
-//     // h1.textContent = dCut[4]
-// }
-
-
 
 setInterval(myTimer, 1000);
 
